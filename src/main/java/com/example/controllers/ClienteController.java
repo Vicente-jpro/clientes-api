9bf8feb939997f6c26694fb1bf7fc9bd1922917f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-06 18:39:23 +0100

    ClienteController added

--- src/main/java/com/example/controllers/ClienteController.java
+++ src/main/java/com/example/controllers/ClienteController.java
@@ -1,5 +1,75 @@
 package com.example.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.models.Cliente;
+import com.example.services.ClienteService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
+	@Autowired
+	private ClienteService clienteService;
+	
+	@PostMapping
+	@ApiOperation("Salvar cliente.")
+	@ApiResponse( code = 201, message = "Cliente criado com sucesso.")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(@RequestBody Cliente cliente) {
+		return this.clienteService.salvar(cliente);
+	}
+	
+	@PatchMapping("/{id_cliente}")
+	@ApiOperation("Atualizar cliente.")
+	@ApiResponse( code = 201, message = "Cliente atualizado com sucesso.")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente atualizar(@RequestBody Cliente cliente, @PathVariable("id_cliente") Integer IdCliente) {
+		Cliente client = this.clienteService.getCliente(IdCliente);
+		cliente.setIdCliente(client.getIdCliente());
+		return this.clienteService.salvar(cliente);
+	}
+	
+	@GetMapping
+	@ApiOperation("Listar todos clientes.")
+	@ApiResponse( code = 302, message = "Clientes encontrados com sucesso.")
+	@ResponseStatus(HttpStatus.FOUND)
+	public List<Cliente> listarClientes() {
+		return this.clienteService.listarClientes();
+	}
+
+	
+	@GetMapping("/{id_cliente}")
+	@ApiOperation("Buscar cliente.")
+	@ApiResponse( code = 302, message = "Cliente encontrado com sucesso.")
+	@ResponseStatus(HttpStatus.FOUND)
+	public Cliente getCliente(@PathVariable("id_cliente") Integer IdCliente) {
+		return this.clienteService.getCliente(IdCliente);
+	}
+	
+	@DeleteMapping("/{id_cliente}")
+	@ApiOperation("Eliminar cliente.")
+	@ApiResponse( code = 302, message = "Cliente encontrado com sucesso.")
+	@ResponseStatus(HttpStatus.FOUND)
+	public void eliminar(@PathVariable("id_cliente") Integer IdCliente) {
+		this.clienteService.eliminar(IdCliente);
+	}
+
+	
+	
 }
