8f09060e3da346d128488ec24da47ab6233d0689
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-06 18:01:42 +0100

    Api erros configured

---
+++ src/main/java/com/example/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,40 @@
+package com.example.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.exceptions.ClienteNotFoundException;
+import com.example.utils.ApiErrors;
+
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+	
+	private String mensagemErro;
+	
+	@ExceptionHandler(ClienteNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ApiErrors clienteNotFoundExceptionHandle(ClienteNotFoundException ex) {
+		this.mensagemErro = ex.getMessage();
+		return new ApiErrors(mensagemErro);
+	}
+	
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors validateFieldsHandle( MethodArgumentNotValidException ex ) {
+		List<String> erros = ex.getBindingResult()
+		  .getAllErrors()
+		  .stream()
+		  .map( erro -> erro.getDefaultMessage()
+				).collect( Collectors.toList());
+		
+		return new ApiErrors(erros);
+	}
+}
---
+++ src/main/java/com/example/exceptions/ClienteNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.exceptions;
+
+public class ClienteNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ClienteNotFoundException(String errorMessage) {
+		super(errorMessage);
+	}
+}
