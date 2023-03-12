c79eb02e3b4735dfcb25008421a9240d0fe16a3e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-12 01:33:18 +0100

    Data cadastro added

--- src/main/java/com/example/services/ClienteService.java
+++ src/main/java/com/example/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.example.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,29 +15,30 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	public Cliente salvar(Cliente cliente) {
 		log.info("ClienteService - salvando cliente. ");
+		cliente.setDataCadastro(LocalDate.now());
 		return this.clienteRepository.save(cliente);
 	}
-	
+
 	public Cliente getCliente(Integer idCliente) {
-		log.info("ClienteService - buscando o cliente com id: "+idCliente);
+		log.info("ClienteService - buscando o cliente com id: " + idCliente);
 		return this.clienteRepository
 				.findById(idCliente)
-				.orElseThrow( () -> new ClienteNotFoundException("Cliente não encontrado. Id invalido :"+idCliente));
+				.orElseThrow(() -> new ClienteNotFoundException("Cliente não encontrado. Id invalido :" + idCliente));
 	}
-	
+
 	public void eliminar(Integer idCliente) {
 		log.info("ClienteService - eliminar cliente.");
-		 Cliente cliente = this.getCliente(idCliente);
-		 this.clienteRepository.delete(cliente);
+		Cliente cliente = this.getCliente(idCliente);
+		this.clienteRepository.delete(cliente);
 	}
-	
-	public List<Cliente> listarClientes(){
+
+	public List<Cliente> listarClientes() {
 		log.info("ClienteService - listando os clientes.");
 		return this.clienteRepository.findAll();
 	}
