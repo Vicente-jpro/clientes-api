b7df1a1d6a1c65ef52a1807e04bbc368e0421642
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-20 10:14:52 +0100

    ServicoPrestadoService

--- src/main/java/com/example/services/ServicoPrestadoService.java
+++ src/main/java/com/example/services/ServicoPrestadoService.java
@@ -1,8 +1,40 @@
 package com.example.services;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.dto.ServicoPrestadoDto;
+import com.example.models.Cliente;
+import com.example.models.ServicoPrestado;
+import com.example.repositories.ClienteRepository;
+import com.example.repositories.ServicoPrestadoRepository;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
 public class ServicoPrestadoService {
 
+    @Autowired
+    private ServicoPrestadoRepository servicoPrestadoRepository;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    public ServicoPrestado salvar(ServicoPrestadoDto servicoPrestadoDto) {
+        log.info("ServicoPrestadoService - Salando a prestação de servico com id_cliente: "
+                + servicoPrestadoDto.getCliente());
+        Cliente cliente = this.clienteService.getCliente(servicoPrestadoDto.getCliente());
+
+        ServicoPrestado servicoPrestado = new ServicoPrestado();
+        servicoPrestado.setCliente(cliente);
+        servicoPrestado.setDescricao(servicoPrestadoDto.getDescricao());
+        servicoPrestado.setValor(servicoPrestadoDto.getValor());
+        servicoPrestado.setData(LocalDate.now());
+
+        ServicoPrestado servico = this.servicoPrestadoRepository.save(servicoPrestado);
+        return servico;
+    }
 }
