6090e77469fb48bbd31b2eaba5bf5078d3c5d677
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-20 09:27:14 +0100

    Relation between cliente and servicoPestado done

--- src/main/java/com/example/models/Cliente.java
+++ src/main/java/com/example/models/Cliente.java
@@ -1,12 +1,15 @@
 package com.example.models;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -30,6 +33,9 @@ public class Cliente {
 	@Column(name = "bi")
 	@NotBlank(message = "Campo BI não pode estar vazio.")
 	private String bi;
+
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+	private List<ServicoPrestado> servicosPrestados;
 
 	@Column(name = "data_cadastro")
 	private LocalDate dataCadastro;
