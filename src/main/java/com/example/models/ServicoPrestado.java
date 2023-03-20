bfa24363caded3528479792e16b57a7a6b4665ee
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-20 09:22:50 +0100

    Entity ServicoPrestado added

---
+++ src/main/java/com/example/models/ServicoPrestado.java
@@ -0,0 +1,37 @@
+package com.example.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "servico_prestado")
+public class ServicoPrestado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer idServicoPrestado;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_servico_prestado")
+    private LocalDate data;
+
+}
