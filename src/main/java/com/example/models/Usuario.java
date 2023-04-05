25e2845190d92b48e383ba28e8f2d9e3a140a4ff
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-04-05 23:38:47 +0100

    usuarios

--- src/main/java/com/example/models/Usuario.java
+++ src/main/java/com/example/models/Usuario.java
@@ -1,15 +1,31 @@
 package com.example.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer idUsuario;
 
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
     private String email;
-    private String password;
+
+    @Column(name = "passwd")
+    private String passwd;
 
 }
