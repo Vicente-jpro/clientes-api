2581f4cff2809369fc8a58cc2c9603f43eb9aca2
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-04-09 18:49:48 +0100

    JwtAuthFilter

--- src/main/java/com/example/securityjwt/JwtAuthFilter.java
+++ src/main/java/com/example/securityjwt/JwtAuthFilter.java
@@ -26,27 +26,30 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	}
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request,
+			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		String authorizationHeader = request.getHeader("Authorization");
+		// String authorizationHeader = request.getHeader("Authorization");
 
-		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
-			String token = authorizationHeader.split(" ")[1];
-			boolean isValid = jwtService.tokenValido(token);
+		// if (authorizationHeader != null && authorizationHeader.startsWith("Bearer"))
+		// {
+		// String token = authorizationHeader.split(" ")[1];
+		// boolean isValid = jwtService.tokenValido(token);
 
-			if (isValid) {
-				String loginUsuario = jwtService.obterLoginUsuario(token);
-				UserDetails userDetails = usuarioService.loadUserByUsername(loginUsuario);
-				UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(userDetails, null,
-						userDetails.getAuthorities());
+		// if (isValid) {
+		// String loginUsuario = jwtService.obterLoginUsuario(token);
+		// UserDetails userDetails = usuarioService.loadUserByUsername(loginUsuario);
+		// UsernamePasswordAuthenticationToken user = new
+		// UsernamePasswordAuthenticationToken(userDetails, null,
+		// userDetails.getAuthorities());
 
-				user.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(user);
-			}
-		}
+		// user.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		// SecurityContextHolder.getContext().setAuthentication(user);
+		// }
+		// }
 
-		filterChain.doFilter(request, response);
+		// filterChain.doFilter(request, response);
 	}
 
 }
