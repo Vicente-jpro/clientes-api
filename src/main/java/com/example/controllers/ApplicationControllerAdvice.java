bae8bbcba99c462f39e20a7de3a9cb0d1f7886b1
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-10 13:11:45 +0100

    New method to handle error added

--- src/main/java/com/example/controllers/ApplicationControllerAdvice.java
+++ src/main/java/com/example/controllers/ApplicationControllerAdvice.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.exceptions.ClienteNotFoundException;
 import com.example.exceptions.DadosInvalidoException;
@@ -62,6 +64,15 @@ public class ApplicationControllerAdvice {
 			UsuarioNotFoundException ex) {
 		this.mensagemErro = ex.getMessage();
 		return new ApiErrors(mensagemErro);
+	}
+
+	@ExceptionHandler(ResponseStatusException.class)
+	public ResponseEntity ResponseStatusExceptionHandle(ResponseStatusException ex) {
+		String messagemErro = ex.getReason();
+		HttpStatus codigoStatus = ex.getStatus();
+		ApiErrors apiErrors = new ApiErrors(messagemErro);
+		return new ResponseEntity(apiErrors, codigoStatus);
+
 	}
 
 	@ExceptionHandler
